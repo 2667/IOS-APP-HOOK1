d778257bb10266b177b88f836f5d97505f11e61f
JikerSun <1206372298@qq.com>
2017-11-30 10:33:05 +0800

    Initial Commit

---
+++ WeChatRedEnvelopesDylib/WeChatRedEnvelopesDylib.h
@@ -0,0 +1,13 @@
+//  weibo: http://weibo.com/xiaoqing28
+//  blog:  http://www.alonemonkey.com
+//
+//  WeChatRedEnvelopesDylib.h
+//  WeChatRedEnvelopesDylib
+//
+//  Created by 触手TV on 2017/11/30.
+//  Copyright (c) 2017年 触手TV. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
